394fffb66c88a1addff271e2cd62f84149ac6bfe
Spencer Smith <ssmith22@usfca.edu>
2016-08-07 14:09:00 -0700

    Working hash search

--- myhtable_search.py
+++ myhtable_search.py
@@ -5,18 +5,45 @@
 from htable import *
 from words import get_text, words
 
-
+"""
+Build an index from word to set of document indexes
+This does the exact same thing as create_index() except that it uses
+your htable.  As a number of htable buckets, use 4011.
+Returns a list-of-buckets hashtable representation.
+"""
 def myhtable_create_index(files):
-    """
-    Build an index from word to set of document indexes
-    This does the exact same thing as create_index() except that it uses
-    your htable.  As a number of htable buckets, use 4011.
-    Returns a list-of-buckets hashtable representation.
-    """
+    wordBook = htable(4011)
+
+    fileIndex = 0
+    for item in files:
+        fileWords = set(words(get_text(item)))
+        for word in fileWords:
+            htable_put(wordBook,word, fileIndex)
+        fileIndex += 1
+    return wordBook
+
+
+"""
+This does the exact same thing as index_search() except that it uses your htable.
+I.e., use htable_get(index, w) not index[w].
+"""
 
 
 def myhtable_index_search(files, index, terms):
-    """
-    This does the exact same thing as index_search() except that it uses your htable.
-    I.e., use htable_get(index, w) not index[w].
-    """
+    validTerms = []
+    for term in terms:
+        if htable_get(index, term)!= None:
+            validTerms += [term]
+
+    if len(validTerms)==0:
+        return []
+
+    overlap = set(htable_get(index, validTerms[0]))
+    for word in validTerms:
+        overlap = set(htable_get(index, word)) & overlap
+
+    searchResults = []
+    for i in list(overlap):
+        searchResults += [list(files)[i]]
+
+    return  searchResults
